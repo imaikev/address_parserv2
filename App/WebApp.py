6c7d7b34a5307b47219de0b63ef8cb63326a97cb
Miguel  Villalba <myke@MacBook-Pro-de-Miguel.local>
2019-02-24 02:11:20 -0300

    v1.0

---
+++ App/WebApp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route("/")
+def main():
+    return "welcome!"
+
+@app.route("/how are you")
+def hello():
+    return "Fine!"
+
+if __name__ == "__main__":
+    app.run (host="0.0.0.0", port= 8080)
+
+
+
+
+
+# In[ ]:
+
+
+
+
