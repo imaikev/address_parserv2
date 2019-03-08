9e002d6760b1ad3c299371bd89250d3e201a3090
imaikev <47906773+imaikev@users.noreply.github.com>
2019-03-08 18:49:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,39 @@
 
 from flask import Flask,jsonify,request
 from validate_email import validate_email
-
-import socket
+import socket ,os , pyodbc , json
 
 app = Flask(__name__)
+
 
 @app.route ('/')
 def home():
     return "Ingrese email: <./mail/someone@server.com> "
 
+	
 @app.route ('/file')
 def file_read():
 	str = open('requirements.txt', 'r').read()
 	return str
+	
+@app.route ('/env')
+def ambiente():
+		data = []
+		new_data = [{'ENGINE': os.environ.get('ENGINE')}] 
+		data.append(new_data)
+		new_data = [{'DATABASE_NAME': os.environ.get('DATABASE_NAME')}] 
+		data.append(new_data)
+		new_data = [{'DATABASE_USER': os.environ.get('DATABASE_USER')}] 
+		data.append(new_data)
+		new_data = [{'DATABASE_PASSWORD': os.environ.get('DATABASE_PASSWORD')}]
+		data.append(new_data)
+		new_data = [{'DATABASE_HOST': os.environ.get('DATABASE_HOST')}] 
+		data.append(new_data)
+		new_data = [{'DATABASE_PORT': os.environ.get('DATABASE_PORT')}] 
+		data.append(new_data)
+		
+		return (jsonify(data))
+
 	
 @app.route ('/mail/<string:mail>')
 def valida_mail(mail):
@@ -35,6 +55,47 @@ def valida_mail_json():
 	y =  [{ 'name': mail , 'status': 'ERROR', 'server': socket.gethostname()}]
 	return jsonify(y)
 
+	
+@app.route ('/DB_VAR')
+def DB_VAR():
+	cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + os.environ.get('DATABASE_HOST') + ','+ os.environ.get('DATABASE_PORT') + ';DATABASE=' + os.environ.get('DATABASE_NAME') + ';UID=' + os.environ.get('DATABASE_USER') + ';PWD=' + os.environ.get('DATABASE_PASSWORD') + '')
+	cursor = cnxn.cursor()
+	cursor.execute("SELECT TOP 100 [de_direccion], de_verificado   FROM [Staging].[dbo].[st_cob_cl_dir_electronica_bkp]")
+	data = []
+	
+	for row in cursor.fetchall():
+		if ( validate_email(row.de_direccion) == True): 
+			   is_valid = 'S'
+		else:
+			   is_valid = 'N'            
+		
+		new_data = [{'mail': row.de_direccion , 'status': is_valid, 'server': socket.gethostname()}]
+		data.append(new_data)
+		
+	
+	return (jsonify(data))
+	
+	
+
+@app.route ('/DB_PLANO')
+def DB_PLANO():
+	cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESAMSSQL22,1148;DATABASE=DWC;UID=DTSDESA;PWD=*Paspa2')
+	cursor = cnxn.cursor()
+	cursor.execute("SELECT TOP 100 [de_direccion], de_verificado   FROM [Staging].[dbo].[st_cob_cl_dir_electronica_bkp]")
+	data = []
+	
+	for row in cursor.fetchall():
+		if ( validate_email(row.de_direccion) == True): 
+			   is_valid = 'S'
+		else:
+			   is_valid = 'N'            
+		
+		new_data = [{'mail': row.de_direccion , 'status': is_valid, 'server': socket.gethostname()}]
+		data.append(new_data)
+		
+	
+	return (jsonify(data))
+	
 
 if __name__ == "__main__":
     app.run (host="0.0.0.0", port= 8080)
