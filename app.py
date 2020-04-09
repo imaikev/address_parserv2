a729a0a5af2992d4dbdb8ce65a15bd0ce2582e99
imaikev <47906773+imaikev@users.noreply.github.com>
2020-04-09 02:03:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 
 from flask import Flask,jsonify,request
 from validate_email import validate_email
+from postal.parser import parse_address
+
 import socket ,os ,  json
 
 app = Flask(__name__)
@@ -10,9 +12,8 @@ app = Flask(__name__)
 
 @app.route ('/')
 def home():
-    return "Ingrese email: <./mail/someone@server.com> "
+    return parse_address('The Book Club 100-106 Leonard St Shoreditch London EC2A 4RH, United Kingdom')
 
-	
 @app.route ('/file')
 def file_read():
 	str = open('requirements.txt', 'r').read()
@@ -55,48 +56,6 @@ def valida_mail_json():
 	y =  [{ 'name': mail , 'status': 'ERROR', 'server': socket.gethostname()}]
 	return jsonify(y)
 
-	
-@app.route ('/DB_VAR')
-def DB_VAR():
-	import pyodbc
-	cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + os.environ.get('DATABASE_HOST') + ','+ os.environ.get('DATABASE_PORT') + ';DATABASE=' + os.environ.get('DATABASE_NAME') + ';UID=' + os.environ.get('DATABASE_USER') + ';PWD=' + os.environ.get('DATABASE_PASSWORD') + '')
-	cursor = cnxn.cursor()
-	cursor.execute("SELECT TOP 100 [de_direccion], de_verificado   FROM [Staging].[dbo].[st_cob_cl_dir_electronica_bkp]")
-	data = []
-	
-	for row in cursor.fetchall():
-		if ( validate_email(row.de_direccion) == True): 
-			   is_valid = 'S'
-		else:
-			   is_valid = 'N'            
-		
-		new_data = [{'mail': row.de_direccion , 'status': is_valid, 'server': socket.gethostname()}]
-		data.append(new_data)
-		
-	
-	return (jsonify(data))
-	
-	
-
-@app.route ('/DB_PLANO')
-def DB_PLANO():
-	import pyodbc
-	cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESAMSSQL22,1148;DATABASE=DWC;UID=DTSDESA;PWD=*Paspa2')
-	cursor = cnxn.cursor()
-	cursor.execute("SELECT TOP 100 [de_direccion], de_verificado   FROM [Staging].[dbo].[st_cob_cl_dir_electronica_bkp]")
-	data = []
-	
-	for row in cursor.fetchall():
-		if ( validate_email(row.de_direccion) == True): 
-			   is_valid = 'S'
-		else:
-			   is_valid = 'N'            
-		
-		new_data = [{'mail': row.de_direccion , 'status': is_valid, 'server': socket.gethostname()}]
-		data.append(new_data)
-		
-	
-	return (jsonify(data))
 	
 
 if __name__ == "__main__":
